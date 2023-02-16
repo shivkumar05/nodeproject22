e54d0871aa7b3fcda0ee1322360b487a5f1bd80e
Aishwarya Sejgaya <aishwaryasejgaya0240@gmail.com>
2023-02-16 12:35:31 +0530

    first commit

---
+++ src/Models/routineModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const drillSchema = new mongoose.Schema({
+    userId: { type: String },
+    routineId: { type: String },
+    drills: { type: String, require: true },
+    date: { type: String, require: true },
+    time: { type: String, require: true },
+    category: { type: Number, require: true },
+    repetation: { type: Number, require: true },
+    sets: { type: Number, require: true },
+    drill_id: { type: String, require: true }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("routine", drillSchema)
